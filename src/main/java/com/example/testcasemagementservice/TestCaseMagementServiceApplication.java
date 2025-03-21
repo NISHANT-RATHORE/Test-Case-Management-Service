1cfe12c624565e2d8ca42af6c39c01e814202900
Nishant Rathore <nishantrathore2002@gmail.com>
2025-03-21 16:08:19 +0530

    first commit

---
+++ src/main/java/com/example/testcasemagementservice/TestCaseMagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcasemagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseMagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCaseMagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testcasemagementservice/TestCaseMagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcasemagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaseMagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
